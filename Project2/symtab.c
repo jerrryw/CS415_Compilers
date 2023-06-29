68dec262c61792c2d990f3fe53193527f5e77158
Jerry Wang <34264605+jerrryw@users.noreply.github.com>
2023-06-29 10:50:41 -0400

    Add files via upload

---
+++ Project2/symtab.c
@@ -0,0 +1,10 @@
+/**********************************************
+        CS415  Compilers  Project 2
+**********************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+extern void * malloc(int size);
+
+
